5a24cb54654161a47b363931e7be8d820578daf7
gCakra <gardagcwa5400@gmail.com>
2025-06-22 21:04:12 +0700

    menambahkan fungsi math sin

--- fungsi.js
+++ fungsi.js
@@ -12,5 +12,8 @@ console.log(multiply(3,8));
 const devide = (a,b) => a/b;
 console.log(devide(6,2));
 
-console.log(add(5,5)); 
+console.log(add(5,5));
+
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(10));
 
