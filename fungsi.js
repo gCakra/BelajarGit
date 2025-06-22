3e8d6cb12688f0e08d59bca716671637405d73c0
gCakra <gardagcwa5400@gmail.com>
2025-06-22 11:01:12 +0700

    menambah fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -3,5 +3,8 @@ console.log("hello world")
 const add = (a,b) => a+b;
 console.log(add(1,2));
 
-const add = (a,b) => a-b;
-console.log(kurang(3,2);
+const kurang = (a,b) => a-b;
+console.log(kurang(3,2));
+
+const multiply = (a,b) => a*b;
+console.log(multiply(3,8));
